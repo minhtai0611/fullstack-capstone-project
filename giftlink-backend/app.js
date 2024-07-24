f8ec40a5f9dbc8b93ef04fe71f725e0040ae19f0
minhtai0611 <minhtainth06111999@gmail.com>
2024-07-24 05:26:23 -0400

    commit

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,56 +1,48 @@
-    require('dotenv').config();
-    const express = require('express');
-    const cors = require('cors');
-    const pinoLogger = require('./logger');
+/*jshint esversion: 8 */
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const pinoLogger = require('./logger');
 
-    const connectToDatabase = require('./models/db');
-    const {loadData} = require("./util/import-mongo/index");
-
-
-    const app = express();
-    app.use("*",cors());
-    const port = 3060;
-
-    // Connect to MongoDB; we just do this one time
-    connectToDatabase().then(() => {
-        pinoLogger.info('Connected to DB');
-    })
-        .catch((e) => console.error('Failed to connect to DB', e));
+const connectToDatabase = require('./models/db');
+const {loadData} = require("./util/import-mongo/index");
 
 
-    app.use(express.json());
+const app = express();
+app.use("*",cors());
+const port = 3060;
 
-    // Route files
-    // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-    const giftRoutes = require('./routes/giftRoutes');
-
-    // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-    //{{insert code here}}
-
-
-    const pinoHttp = require('pino-http');
-    const logger = require('./logger');
-
-    app.use(pinoHttp({ logger }));
-
-    // Use Routes
-    // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-    app.use('/api/gifts', giftRoutes);
-
-    // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-    //{{insert code here}}
+// Connect to MongoDB; we just do this one time
+connectToDatabase().then(() => {
+    pinoLogger.info('Connected to DB');
+})
+    .catch((e) => console.error('Failed to connect to DB', e));
 
 
-    // Global Error Handler
-    app.use((err, req, res, next) => {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
-    });
+app.use(express.json());
 
-    app.get("/",(req,res)=>{
-        res.send("Inside the server")
-    })
+// Route files
+const giftRoutes = require('./routes/giftRoutes');
+const searchRoutes = require('./routes/searchRoutes');
+const pinoHttp = require('pino-http');
+const logger = require('./logger');
 
-    app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
-    });
+app.use(pinoHttp({ logger }));
+
+// Use Routes
+app.use('/api/gifts', giftRoutes);
+app.use('/api/search', searchRoutes);
+
+// Global Error Handler
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+});
+
+app.get("/",(req,res)=>{
+    res.send("Inside the server")
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -1,42 +1,39 @@
-const express = require('express');
-const router = express.Router();
-const connectToDatabase = require('../models/db');
+    const express = require('express');
+    const router = express.Router();
+    const connectToDatabase = require('../models/db');
 
-// Search for gifts
-router.get('/', async (req, res, next) => {
-    try {
-        // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
-        // {{insert code here}}
+    // Search for gifts
+    router.get('/', async (req, res, next) => {
+        try {
 
-        const collection = db.collection("gifts");
+            // Task 1: Connect to MongoDB
+            const db = await connectToDatabase();
+            const collection = db.collection("gifts");
+            // Initialize the query object
+            let query = {};
 
-        // Initialize the query object
-        let query = {};
+            // Task 2: check if the name exists and is not empty
+            if (req.query.name && req.query.name.trim() !== '') {
+                query.name = { $regex: req.query.name, $options: "i" }; // Using regex for partial match, case-insensitive
+            }
 
-        // Add the name filter to the query if the name parameter is not empty
-        // if (/* {{insert code here}} */) {
-            query.name = { $regex: req.query.name, $options: "i" }; // Using regex for partial match, case-insensitive
-        // }
+            // Task 3: Add other filters to the query
+            if (req.query.category) {
+                query.category = req.query.category;
+            }
+            if (req.query.condition) {
+                query.condition = req.query.condition;
+            }
+            if (req.query.age_years) {
+                query.age_years = { $lte: parseInt(req.query.age_years) };
+            }
 
-        // Task 3: Add other filters to the query
-        if (req.query.category) {
-            // {{insert code here}}
+            // Task 4: Fetch filtered gifts
+            const gifts = await collection.find(query).toArray();
+            res.json(gifts);
+        } catch (e) {
+            next(e);
         }
-        if (req.query.condition) {
-            // {{insert code here}} 
-        }
-        if (req.query.age_years) {
-            // {{insert code here}}
-            query.age_years = { $lte: parseInt(req.query.age_years) };
-        }
+    });
 
-        // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
-        // {{insert code here here}}
-
-        res.json(gifts);
-    } catch (e) {
-        next(e);
-    }
-});
-
-module.exports = router;
+    module.exports = router;
