bae179a0fb4b3a456c44764913658ee7ec4202a7
minhtai0611 <minhtainth06111999@gmail.com>
2024-07-25 04:14:17 -0400

    commit

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -8,6 +8,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import DetailsPage from './components/DetailsPage/DetailsPage';
+import SearchPage from './components/SearchPage/SearchPage';
 function App() {
   return (
         <>
@@ -19,6 +20,7 @@ function App() {
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/profile" element={<Profile/>} />
           <Route path="/app/product/:productId" element={<DetailsPage/>} />
+          <Route path="/app/search" element={<SearchPage/>} />
         </Routes>
         </>
   );
